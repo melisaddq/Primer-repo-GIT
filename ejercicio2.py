70a966e430249d6f6c6d9f83980c765144b35931
Melisa <mely_ddq@hotmail.com>
2025-03-30 20:42:45 -0300

    agrego modificacion desde nuevarama

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+# Empezamos pidiendo al usuario que ingrese su nombre y almacenando su respuesta en la variable "nombre"
+nombre = input("Por favor, ingrese su nombre: ")
+
+# Imprimimos el saludo utilizando printf
+print(f"Hola {nombre}!")
+
+# Otra alternativa sin usar printf sería
+print("Hola " + nombre + "!")
